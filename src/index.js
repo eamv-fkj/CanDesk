d8f921aa1e5cf197b6573a22a82b35bc2f928d92
Flemming K. Jensen <flemming.k.jensen@gmail.com>
2024-03-31 18:47:23 +0200

    Fullscreen test placed in function in index.js, style removed from styles.css, and set from JavaScript. Also button is now created from JavaScript and body of html starts empty.

--- src/index.js
+++ src/index.js
@@ -1,20 +1,31 @@
 import generate from './generate'
 import Fullscreen from './fullscreen'
 
-const toggleFullscreenButton = document.getElementById('toggleFullscreenButtonID');
 
-if (Fullscreen.isSupported()) {
-  // fullscreen starts off
-  toggleFullscreenButton.textContent = 'Enter Fullscreen' + ': ' + generate();
 
-  toggleFullscreenButton.addEventListener('click', function() {
-    Fullscreen.toggle();
-  });
+function testFullscreen() {
+  let toggleFullscreenButton = document.createElement('button');
+  toggleFullscreenButton.id = 'toggleFullscreenButtonID';
+  toggleFullscreenButton.style.width = '200px';
+  toggleFullscreenButton.style.height = '50px';
+  toggleFullscreenButton.style.fontSize = '18px';
+  document.body.appendChild(toggleFullscreenButton);
 
-  Fullscreen.addEventListener(function(enabled) {
-    toggleFullscreenButton.textContent = (enabled ? 'Exit' : 'Enter') + ' Fullscreen';
-  });
+  if (Fullscreen.isSupported()) {
+    // fullscreen starts off
+    toggleFullscreenButton.textContent = 'Enter Fullscreen' + ': ' + generate();
+  
+    toggleFullscreenButton.addEventListener('click', function() {
+      Fullscreen.toggle();
+    });
+  
+    Fullscreen.addEventListener(function(enabled) {
+      toggleFullscreenButton.textContent = (enabled ? 'Exit' : 'Enter') + ' Fullscreen';
+    });
+  
+  } else
+    // fullscreen not supported
+    toggleFullscreenButton.remove();
+}
 
-} else
-  // fullscreen not supported
-  toggleFullscreenButton.remove();
+testFullscreen();
