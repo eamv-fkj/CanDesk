bc512fbda773d68ad39cd1f58ccc787ba6b87e77
Flemming K. Jensen <flemming.k.jensen@gmail.com>
2024-04-04 19:38:59 +0200

    Added the script 'deploy' to deploy the build to the ftp-server. Use: npm run deploy, to run the script, and read 'deploy.js' for more information.

---
+++ deploy.js
@@ -0,0 +1,45 @@
+/*
+ * this will deploy the build project to the ftp server
+ * using: npm run deploy
+ * (see: package.json, for the script)
+ * 
+ * FTP host, user and password is set in enviroments vars
+ * in the './.env' file (_not_ commited to git!)
+ */
+
+// https://www.npmjs.com/package/basic-ftp
+const { Client } = require("basic-ftp");
+
+// https://www.npmjs.com/package/dotenv
+require('dotenv').config();
+
+async function deploy() {
+    let client = new Client();
+
+    // http://fkj.dk/test/candesk.js/index.html
+    let remoteDir = '/www/test/candesk.js/';
+
+    let localDir = './dist/';
+
+    try {
+      await client.access({
+          host:     process.env.CANDESK_JS_FTP_HOST,
+          user:     process.env.CANDESK_JS_FTP_USER,
+          password: process.env.CANDESK_JS_FTP_PASSWORD,
+          secure:   false // Set to true if using FTPS
+      });
+      await client.removeDir(remoteDir, true);
+      await client.ensureDir(remoteDir);
+      await client.cd(remoteDir);
+      await client.uploadFromDir(localDir);
+
+      console.log("Deployment succeeded!");
+
+    } catch (error) {
+      console.error("Deployment failed:", error);
+    }
+
+    client.close();
+}
+
+deploy();
